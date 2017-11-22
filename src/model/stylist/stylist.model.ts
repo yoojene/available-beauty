d1a0e97919b8c4a77796b371e6bdec93c52386a2
Eugene <eugene.cross@gmail.com>
2017-11-22 12:54:12 +0000

    Stylist interface

---
+++ src/model/stylist/stylist.model.ts
@@ -0,0 +1,13 @@
+export interface Stylist {
+  name: string;
+  baseLocation: number[];
+  mobile: boolean;
+  mobileRange?: number;
+  bio?: string;
+  phoneNumber?: string;
+  emailAddress: string;
+  avatarImage?: string;
+  bannerImage?: string;
+  galleryImages?: string[];
+
+}
