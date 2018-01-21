3e8c9c4c9106ea4b30c68615fb511b54fadc0aab
Eugene <eugene.cross@gmail.com>
2018-01-21 14:54:57 +0000

    Starting the StylistRegister form

--- src/pages/stylist-register/stylist-register.ts
+++ src/pages/stylist-register/stylist-register.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the StylistRegisterPage page.
@@ -14,13 +15,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'stylist-register.html'
 })
 export class StylistRegisterPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public stylistNameLabel = 'Stylist Name';
+  public bioLabel = 'Write a few details here about your salon';
+  public mobileLabel = 'Are you a mobile stylist?';
+  public mobileRangeLabel = 'How far will you travel from your base?';
+  public loadImagesLabel = 'Upload gallery images now?';
+  public stylistRegForm: any;
+  public showMobileRange: boolean;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+    this.stylistRegForm = formBuilder.group({
+      // phoneNumber: ['', Validators.required],
+      stylistName: ['', Validators.required],
+      bio: [''],
+      mobile: [false, Validators.required],
+      mobileRange: [''],
+      loadImages: [false, Validators.required]
+      // galleryImages: ['']
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StylistRegisterPage');
+    this.stylistRegForm.get('mobile').valueChanges.subscribe(val => {
+      console.log(val);
+      this.showMobileRange = val;
+    });
+    // console.log(this.showMobileRange);
   }
 
   goToHome() {
     this.navCtrl.push('TabsPage');
+  }
+
+  completeStylistReg() {
+    console.log('Stylist reged!');
   }
 }
