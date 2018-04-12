8549b549c845e230ab6b17627e85bd8e161ea11c
Philip Leaper <philipleaper@gmail.com>
2018-04-12 23:17:06 +0100

    updates to edit profile page

--- src/pages/edit-user-profile/edit-user-profile.ts
+++ src/pages/edit-user-profile/edit-user-profile.ts
@@ -2,6 +2,8 @@ import { Component, AfterContentInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StylistProvider } from '../../providers/stylist/stylist';
 import * as firebase from 'firebase';
+import { PhotoProvider } from '../../providers/photo/photo';
+
 //import { UtilsProvider } from '../../providers/utils/utils';
 //import { Observable } from 'rxjs/Observable';
 
@@ -30,12 +32,16 @@ export class EditUserProfilePage implements AfterContentInit {
   addressCounty = 'County';
   addressPostcode = 'Postcode';
   bio = 'Bio';
+
+  public loadProgress: any = 0;
+  public downloadUrls: Array<any> = [];
   
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private stylist: StylistProvider,
+    public photo: PhotoProvider,
     //private utils: UtilsProvider
   ) {
 
@@ -78,6 +84,62 @@ export class EditUserProfilePage implements AfterContentInit {
         console.log(obj);
       });
 
+  }
+
+  doEditBanner() {
+    console.log('Getting photo from picker');
+    this.photo.getLibraryPictures().then(res => {
+      let returnedPhoto: any = res;
+      this.photo
+        .getBase64Data(returnedPhoto.photoFullPath, returnedPhoto.path)
+        .then(baseress => {
+          console.log(baseress);
+          this.photo.pushPhotoToStorage(baseress).then(stores => {
+            console.log(stores[0]);
+            this.monitorUploadProgress(stores[0]);
+          });
+        });
+    });
+
+    //UPDATE IMAGE ON PAGE
+  }
+
+  public monitorUploadProgress(tasks) {
+    console.log('monitorUploadProgress');
+
+    tasks.forEach(task => {
+      console.log(task);
+      task.on(
+        'state_changed',
+        (snapshot: any) => {
+          this.loadProgress = (
+            snapshot.bytesTransferred /
+            snapshot.totalBytes *
+            100
+          ).toFixed(2);
+          // this.loadProgress.push(prog);
+          // console.log(this.loadProgress);
+          console.log('Upload is ' + this.loadProgress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+          // return progress;
+        },
+        err => {
+          console.error(err);
+        },
+        () => {
+          console.log('success!');
+          // Need the URLs for RTDB update
+          this.downloadUrls.push(task.snapshot.downloadURL);
+        }
+      );
+    });
   }
 
   // ngAfterViewInit() {
